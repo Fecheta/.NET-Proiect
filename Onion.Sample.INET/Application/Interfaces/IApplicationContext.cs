5bb29af8cdc0f6501748843b22ef9a2667c6eea0
Fecheta Virgil-Vicentiu <75781128+Fecheta@users.noreply.github.com>
2021-11-10 21:05:32 +0200

    Architecture 01

---
+++ Onion.Sample.INET/Application/Interfaces/IApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationContext
+    {
+        DbSet<Domain.Entities.House> Products { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Onion.Sample.INET/Persistence/Context/HouseContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context
+{
+    public class HouseContext : DbContext
+    {
+        //public ProductContext()
+        //{
+
+        //}
+        public HouseContext(DbContextOptions<HouseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<House> Houses { get ; set; }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("Data Source=MyProducts.db");
+        // }
+    }
+}
