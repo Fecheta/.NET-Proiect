52b14ac2889d103a142732a17c35d577cf587100
Cristian Fartade <fartade.cristian@gmail.com>
2021-12-01 23:46:05 +0200

    added house model

--- PricePrediction/ClientApp/src/app/addHouse/addHouse.component.ts
+++ PricePrediction/ClientApp/src/app/addHouse/addHouse.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { House } from '../models/house';
 
 @Component({
     selector: 'add-house',
@@ -9,8 +12,33 @@ import { Router } from '@angular/router';
   })
 
   export class AddHouseComponent{
+
+    houseModel:House = {
+      id: 1,
+      bedrooms: "string",
+      squareFeet: "string",
+      floors: "string",
+      zipcode: "string",
+      yearbuilt: "string",
+      price:"string",
+    }
+
     constructor(
         private router: Router,
+        private httpClient: HttpClient,
+      ) {
+        httpClient.post<number>('/api/1.0/Houses', this.houseModel).subscribe(result =>{
+          console.log(result);
+        }
+          )
+      }
 
-      ) {}
+      // add(house: House):Observable<number> {
+      //   console.log('aci');
+
+      //   var result = this.httpClient.post<number>("/api/1.0/Houses", house)
+
+
+      //   return result;
+      // }
   }
