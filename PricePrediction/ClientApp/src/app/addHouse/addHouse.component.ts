4e669eb2d5268ea948c1e006c08c6745f18e3399
Cristian Fartade <fartade.cristian@gmail.com>
2021-12-02 01:20:11 +0200

    added validations for input fields

--- PricePrediction/ClientApp/src/app/addHouse/addHouse.component.ts
+++ PricePrediction/ClientApp/src/app/addHouse/addHouse.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { House } from '../models/house';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 @Component({
     selector: 'add-house',
@@ -12,33 +13,63 @@ import { House } from '../models/house';
   })
 
   export class AddHouseComponent{
+    inputForm: FormGroup = new FormGroup({});
+    control = new FormControl('', Validators.required);
+    invalidForm = false;
 
     houseModel:House = {
       id: 1,
-      bedrooms: "string",
-      squareFeet: "string",
-      floors: "string",
-      zipcode: "string",
-      yearbuilt: "string",
-      price:"string",
+      bedrooms: "",
+      bathrooms: "",
+      squareFeet: "",
+      floors: "",
+      zipcode: "",
+      yearbuilt: "",
+      price:"",
     }
 
     constructor(
         private router: Router,
         private httpClient: HttpClient,
+        formBuilder: FormBuilder
       ) {
-        httpClient.post<number>('/api/1.0/Houses', this.houseModel).subscribe(result =>{
-          console.log(result);
+        this.inputForm = formBuilder.group({
+          price: ['', [Validators.required, Validators.pattern('^[0-9]*$'), ]],
+          zipcode: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(5), Validators.maxLength(5)]],
+          yearbuilt: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1800), Validators.max((new Date()).getFullYear())]],
+          bedrooms: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1)]],
+          bathrooms: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1)]],
+          squarefeet: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(20)]],
+          floors: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1)]],
+        });
+      }
+      get f() {
+        return this.inputForm.controls;
+      }
+    
+      submit() {
+        if (!this.inputForm.invalid) {
+          console.log(this.inputForm.value);
         }
-          )
+      }
+    
+      compute() {
+        if (!this.inputForm.invalid) {
+          this.invalidForm = false;
+          this.router.navigateByUrl('waitingPage');
+        } else {
+          this.invalidForm = true;
+        }
+      }
+    
+      setInvalidForm() {
+        this.invalidForm = false;
       }
 
-      // add(house: House):Observable<number> {
-      //   console.log('aci');
+      add(house: House) {
+        var result = this.httpClient.post<number>('/api/1.0/Houses', this.houseModel).subscribe(result =>{
+          console.log(result);
+        });
 
-      //   var result = this.httpClient.post<number>("/api/1.0/Houses", house)
-
-
-      //   return result;
-      // }
+      }
   }
