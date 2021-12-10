f014322a614d9fe6373f3e946478e629a20461b1
Cristian Fartade <fartade.cristian@gmail.com>
2021-12-10 12:01:56 +0200

    added validations

--- Application/Features/Commands/CreateHouseCommand.cs
+++ Application/Features/Commands/CreateHouseCommand.cs
@@ -26,12 +26,35 @@ namespace Application.Features.Commands
         public CreateHouseCommandValidator()
         {
             RuleFor(x => x.Bedrooms)
+              .NotEmpty()
+              .Matches(@"[1-9][0-9]*");
+
+            RuleFor(x => x.Bathrooms)
                 .NotEmpty()
                 .Matches(@"[1-9][0-9]*");
 
             RuleFor(x => x.Price)
                 .NotEmpty()
-                .GreaterThan("2000");
+                .GreaterThan("2000")
+                .Matches(@"[1-9][0-9]*");
+
+            RuleFor(x => x.Floors)
+                .NotEmpty()
+                .GreaterThan("1")
+                .Matches(@"[1-9][0-9]*");
+
+            RuleFor(x => x.ZipCode)
+                .NotEmpty()
+                .Matches(@"[1-9][1-9][1-9][1-9][1-9]");
+
+            RuleFor(x => x.SquareFeet)
+                .NotEmpty()
+                .GreaterThan("100")
+                .Matches(@"[1-9][0-9]*");
+
+            RuleFor(x => x.YearBuilt)
+                .NotEmpty()
+                .InclusiveBetween("1900", DateTime.Now.Year.ToString());
         }
     }
 }
--- Tests/Controllers/Validators/CreateHouseCommandValidatorTests.cs
+++ Tests/Controllers/Validators/CreateHouseCommandValidatorTests.cs
@@ -53,5 +53,106 @@ namespace Tests.Controllers.Validators
            var result=  _validator.TestValidate(model);
             result.ShouldHaveValidationErrorFor(x => x.Bedrooms);
         }
+        [Test]
+        public void WhenBathroomsIsMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<CreateHouseCommand>()
+                .With(x => x.Bedrooms, "2")
+                .Without(x => x.Bathrooms)
+                .With(x => x.SquareFeet, "200")
+                .With(x => x.Floors, "2")
+                .With(x => x.ZipCode, "22222")
+                .With(x => x.YearBuilt, "2000")
+                .With(x => x.Price, "2000000")
+                .Create();
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Bathrooms);
+        }
+
+        [Test]
+        public void WhenSquarefeetIsMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<CreateHouseCommand>()
+                .With(x => x.Bedrooms, "2")
+                .With(x => x.Bathrooms, "2")
+                .Without(x => x.SquareFeet)
+                .With(x => x.Floors, "2")
+                .With(x => x.ZipCode, "22222")
+                .With(x => x.YearBuilt, "2000")
+                .With(x => x.Price, "2000000")
+                .Create();
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.SquareFeet);
+        }
+
+        [Test]
+        public void WhenFloorsIsMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<CreateHouseCommand>()
+                .With(x => x.Bedrooms, "2")
+                .With(x => x.Bathrooms, "2")
+                .With(x => x.SquareFeet, "250")
+                .Without(x => x.Floors)
+                .With(x => x.ZipCode, "22222")
+                .With(x => x.YearBuilt, "2000")
+                .With(x => x.Price, "2000000")
+                .Create();
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Floors);
+        }
+
+        [Test]
+        public void WhenZipCodeIsMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<CreateHouseCommand>()
+                .With(x => x.Bedrooms, "2")
+                .With(x => x.Bathrooms, "2")
+                .With(x => x.SquareFeet, "250")
+                .With(x => x.Floors, "3")
+                .Without(x => x.ZipCode)
+                .With(x => x.YearBuilt, "2000")
+                .With(x => x.Price, "2000000")
+                .Create();
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.ZipCode);
+        }
+
+        [Test]
+        public void WhenYearBuiltIsMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<CreateHouseCommand>()
+                .With(x => x.Bedrooms, "2")
+                .With(x => x.Bathrooms, "2")
+                .With(x => x.SquareFeet, "250")
+                .With(x => x.Floors, "3")
+                .With(x => x.ZipCode, "12345")
+                .Without(x => x.YearBuilt)
+                .With(x => x.Price, "2000000")
+                .Create();
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.YearBuilt);
+        }
+        [Test]
+        public void WhenPriceIsMissing_ShouldReturnError()
+        {
+            var model = _fixture.Build<CreateHouseCommand>()
+                .With(x => x.Bedrooms, "2")
+                .With(x => x.Bathrooms, "2")
+                .With(x => x.SquareFeet, "250")
+                .With(x => x.Floors, "3")
+                .With(x => x.ZipCode, "12345")
+                .With(x => x.YearBuilt, "2011")
+                .Without(x => x.Price)
+                .Create();
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Price);
+        }
+
     }
 }
