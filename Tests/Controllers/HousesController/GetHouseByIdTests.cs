ac801d4bda62d03532acf040f62ff79cb6be30ff
Cristian Fartade <fartade.cristian@gmail.com>
2021-12-17 00:02:50 +0200

    added GetHouseByIdTests

---
+++ Tests/Controllers/HousesController/GetHouseByIdTests.cs
@@ -0,0 +1,59 @@
+﻿using Application.Features.Queries;
+using Domain.Entities;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebAPI.Controllers.v1;
+
+namespace Tests.Controllers.HouseController
+{
+    [TestFixture]
+    public class GetHouseByIdTests
+    {
+        private HousesController _controller;
+        private Mock<IMediator> _mediator;
+        private GetHousesQuery _request;
+
+        [SetUp]
+        public void Init()
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new HousesController(_mediator.Object);
+            
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            _controller = null;
+        }
+
+        [Test]
+        public async Task ShouldSendGetHouseByIdQuery()
+        {
+            _mediator.Setup(m => m.Send(It.IsAny<GetHouseByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(new House()));
+
+            var result = await _controller.GetById(5);
+
+            _mediator.Verify(m => m.Send(It.IsAny<GetHouseByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test]
+        public async Task ShouldReturnStatusOk_WhenRequestCompletesSuccessfully()
+        {
+            var result = await _controller.GetById(5);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+    }
+}
