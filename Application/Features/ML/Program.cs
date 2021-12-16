3f7713d9a0be6921aebd49b6c71443758d169714
martiscafilip <martisca_filip@yahoo.com>
2021-12-16 16:44:07 +0200

    alg train added

--- Application/Features/ML/Program.cs
+++ Application/Features/ML/Program.cs
@@ -6,84 +6,108 @@ using Domain.Entities;
 using Microsoft.ML;
 using System.Threading.Tasks;
 using Application.Features.Utils;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
 
 namespace PricePrediction
 {
-    public class Program 
+    public class Program
     {
-        private static readonly string TRAINING_DATA_FILEPATH 
+        private static readonly string TRAINING_DATA_FILEPATH
             = "..\\Application\\Features\\ML\\kc_house_data.csv";
-        private static readonly string MODEL_FILEPATH 
+        private static readonly string MODEL_FILEPATH
             = "..\\Application\\Features\\ML\\MLModel.zip";
 
         public async Task<float> PricePredict(PricePredictUtil data)
         {
             MLContext mlContext = new MLContext(1);
-             mlContext.Data.LoadFromTextFile<ModelInput>(
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
                 path: TRAINING_DATA_FILEPATH,
                 hasHeader: true,
-                separatorChar:',',
-                allowQuoting:true,
+                separatorChar: ',',
+                allowQuoting: true,
                 allowSparse: false);
 
-             mlContext.Transforms.Categorical.OneHotEncoding(new[]
+            var dataProcessPipeline = mlContext.Transforms.Categorical.OneHotEncoding(new[]
             {
                 new InputOutputColumnPair("date", "date"),
                 new InputOutputColumnPair("floors", "floors"),
                 new InputOutputColumnPair("zipcode", "zipcode")
             }).Append(mlContext.Transforms.Concatenate("Features", new[]
             {
-                "date",
                 "bedrooms",
                 "bathrooms",
                 "sqft_living",
-                "sqft_lot",
                 "floors",
-                "waterfront",
-                "view",
-                "condition",
-                "grade",
-                "sqft_above",
-                "sqft_basement",
                 "yr_built",
-                "yr_renovated",
-                "zipcode",
-                "lat",
-                "long",
-                "sqft_living15",
-                "sqft_lot15"
+                "zipcode"
+
             }));
+
+            //UNCOMMENT FOR TRAINING
+
+            //var settings = new RegressionExperimentSettings()
+            //{
+            //    MaxExperimentTimeInSeconds = 10
+            //};
+            //var experiment = mlContext.Auto().CreateRegressionExperiment(settings);
+            //var progress = new Progress<RunDetail<RegressionMetrics>>(p =>
+            //{
+            //    if (p.ValidationMetrics != null)
+            //    {
+            //        Console.Write($"Current result: {p.TrainerName}");
+            //        Console.Write($"      {p.ValidationMetrics.RSquared}");
+            //        Console.Write($"      {p.ValidationMetrics.MeanAbsoluteError}");
+            //        Console.WriteLine();
+            //    }
+            //});
+
+            //var result = experiment.Execute(trainingDataView, labelColumnName: "price", progressHandler: progress);
+
+            //Console.WriteLine("Best run:");
+            //Console.WriteLine($"Trainer name - {result.BestRun.TrainerName}");
+            //Console.WriteLine($"RSquared - {result.BestRun.ValidationMetrics.RSquared}");
+            //Console.WriteLine($"MAE - {result.BestRun.ValidationMetrics.MeanAbsoluteError}");
+
+
+            //var trainer = mlContext.Regression.Trainers.LightGbm(labelColumnName: "price", featureColumnName: "Features");
+            //var trainingPipeline = dataProcessPipeline.Append(trainer);
+            //ITransformer model = trainingPipeline.Fit(trainingDataView);
+
+            //mlContext.Model.Save(model,trainingDataView.Schema,MODEL_FILEPATH);
+
+
+            //id,date,price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,condition,grade,sqft_above,sqft_basement,yr_built,yr_renovated,zipcode,lat,long,sqft_living15,sqft_lot15
+            //"7129300520","20141013T000000",221900,3,1,1180,5650,"1",0,0,3,7,1180,0,1955,0,"98178",47.5112,-122.257,1340,5650
+
 
             ModelInput sampleData = new ModelInput()
             {
-                date = "20141013T000000",
                 Bedrooms = data.Bedrooms,
                 Bathrooms = data.Bedrooms,
                 Sqft_living = data.SquareFeet,
-                Sqft_lot = 5650F,
                 Floors = data.Floors,
-                Waterfront = 0F,
-                View = 0F,
-                Condition = 3F,
-                Grade = 7F,
-                Sqft_above = 1180F,
-                Sqft_basement = 0F,
                 Yr_built = data.YearBuit,
-                Yr_renovated = 0F,
                 Zipcode = data.Zipcode,
-                Lat = 47.5112F,
-                Long = -122.257F,
-                Sqft_living15 = 1340F,
-                Sqft_lot15 = 5650F
-
             };
 
             ITransformer mlModel = mlContext.Model.Load(MODEL_FILEPATH, out var modelInputSchema);
 
             var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
 
-            ModelOutput pResult = predEngine.Predict(sampleData);
-            
+            ModelOutput pResult = new ModelOutput();
+
+            await Task.Run(() =>
+            {
+                 pResult = predEngine.Predict(sampleData);
+            });
+
             Console.WriteLine($"Predicted price: {pResult.Score}");
 
             return pResult.Score;
--- Application/Features/Utils/PricePredictUtilHandler.cs
+++ Application/Features/Utils/PricePredictUtilHandler.cs
@@ -9,32 +9,16 @@ namespace Application.Features.Utils
 {
     public class PricePredictUtilHandler : IRequestHandler<PricePredictUtil, float>
     {
-        private readonly IHouseRepository repository;
         private readonly Program program = new();
 
-        public PricePredictUtilHandler(IHouseRepository repository)
-        {
-            this.repository = repository;
-        }
         public async Task<float> Handle(PricePredictUtil request, CancellationToken cancellationToken)
         {
-            /*var entity = await repository.GetByZipcodeAndBedrooms(request.Zipcode, request.Bedrooms);
-            
-            if(entity is null)
-            {
-                return -1;
-            }
-            return decimal.Parse(entity.Price);*/
+            var price = await program.PricePredict(request);
 
 
-
-             var price = await program.PricePredict(request);
-
-
-            if (price == null) return -1;
+            if (price <= 1) return -1;
             else return price;
 
-            
         }
     }
 }
